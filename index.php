81b7d3eb0fdc51b7319b23f4ef0b7b398d6ca046
Susan <susan01vasquez@gmail.com>
2024-10-30 20:28:49 -0600

    Realización basica del formulario

--- index.php
+++ index.php
@@ -6,6 +6,18 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="" method="post">
+        <div>
+            <label for="">User name or email address</label>
+            <input type="text" name="username">
+        </div>
+        <div>
+            <label for="">Password</label>
+            <input type="password" name = "password">
+        </div>
+        <div>
+            <input type="submit" value ="Enviar">
+        </div>
+    </form>
 </body>
 </html>
